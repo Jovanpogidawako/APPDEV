3640a85f80bb3c233c5131c03fdcfc9ab46b1325
DarylBM <135843760+Jovanpogidawako@users.noreply.github.com>
2023-12-14 08:00:24 +0800

    Update Messaging.vue

--- carapp/src/pages/Main/Messaging.vue
+++ carapp/src/pages/Main/Messaging.vue
@@ -149,13 +149,14 @@
 
       <q-page-container class="bg-grey-2">
         <router-view />
+        <chat :currentConversation="currentConversation" />
       </q-page-container>
 
       <q-footer>
         <q-toolbar class="bg-grey-3 text-black row">
           <q-btn round flat icon="insert_emoticon" class="q-mr-sm" />
           <q-input rounded outlined dense class="WAL__field col-grow q-mr-sm" bg-color="white" v-model="message" placeholder="Type a message" />
-          <q-btn round flat icon="mic" />
+          <q-btn round flat icon="mic" @click="sendMessage" />
         </q-toolbar>
       </q-footer>
     </q-layout>
@@ -163,8 +164,9 @@
 </template>
 
 <script>
-import { useQuasar } from 'quasar'
-import { ref, computed } from 'vue'
+import { useQuasar } from 'quasar';
+import { ref, computed } from 'vue';
+
 
 const conversations = [
   {
@@ -173,59 +175,54 @@ const conversations = [
     avatar: 'https://cdn.quasar.dev/team/razvan_stoenescu.jpeg',
     caption: 'I\'m working on Quasar!',
     time: '15:00',
-    sent: true
+    sent: true,
+    messages: [
+      { text: 'Hi Razvan!', time: '15:01', sent: false },
+      { text: 'How are you?', time: '15:02', sent: true },
+    ],
   },
-  {
-    id: 2,
-    person: 'Dan Popescu',
-    avatar: 'https://cdn.quasar.dev/team/dan_popescu.jpg',
-    caption: 'I\'m working on Quasar!',
-    time: '16:00',
-    sent: true
-  },
-  {
-    id: 3,
-    person: 'Jeff Galbraith',
-    avatar: 'https://cdn.quasar.dev/team/jeff_galbraith.jpg',
-    caption: 'I\'m working on Quasar!',
-    time: '18:00',
-    sent: true
-  },
-  {
-    id: 4,
-    person: 'Allan Gaunt',
-    avatar: 'https://cdn.quasar.dev/team/allan_gaunt.png',
-    caption: 'I\'m working on Quasar!',
-    time: '17:00',
-    sent: true
-  }
-]
+  // ... other conversations
+];
 
 export default {
   name: 'WhatsappLayout',
 
-  setup () {
-    const $q = useQuasar()
+  setup() {
+    const $q = useQuasar();
 
-    const leftDrawerOpen = ref(false)
-    const search = ref('')
-    const message = ref('')
-    const currentConversationIndex = ref(0)
+    const leftDrawerOpen = ref(false);
+    const search = ref('');
+    const message = ref('');
+    const currentConversationIndex = ref(0);
 
     const currentConversation = computed(() => {
-      return conversations[ currentConversationIndex.value ]
-    })
+      return conversations[currentConversationIndex.value];
+    });
 
     const style = computed(() => ({
-      height: $q.screen.height + 'px'
-    }))
+      height: $q.screen.height + 'px',
+    }));
 
-    function toggleLeftDrawer () {
-      leftDrawerOpen.value = !leftDrawerOpen.value
+    function toggleLeftDrawer() {
+      leftDrawerOpen.value = !leftDrawerOpen.value;
     }
 
-    function setCurrentConversation (index) {
-      currentConversationIndex.value = index
+    function setCurrentConversation(index) {
+      currentConversationIndex.value = index;
+    }
+
+    function sendMessage() {
+      if (message.value.trim() === '') return;
+
+      const newMessage = {
+        text: message.value,
+        time: new Date().toLocaleTimeString(),
+        sent: true,
+      };
+
+      currentConversation.value.messages.push(newMessage);
+
+      message.value = '';
     }
 
     return {
@@ -234,17 +231,15 @@ export default {
       message,
       currentConversationIndex,
       conversations,
-
       currentConversation,
       setCurrentConversation,
       style,
-
-      toggleLeftDrawer
-    }
-  }
-}
+      toggleLeftDrawer,
+      sendMessage,
+    };
+  },
+};
 </script>
-
 
 <style lang="sass">
 .WAL
@@ -264,9 +259,6 @@ export default {
     margin: 0 auto
     z-index: 4000
     height: 100%
-    /* Remove the width and max-width properties */
-    /* width: 90% */
-    /* max-width: 950px */
     border-radius: 5px
 
   &__field.q-field--outlined .q-field__control:before
@@ -276,24 +268,22 @@ export default {
     .WAL__drawer-close
       display: none
 
-@media (max-width: 850px)
-  .WAL
-    padding: 0
-    &__layout
-      width: 100%
-      border-radius: 0
+  @media (max-width: 850px)
+    .WAL
+      padding: 0
+      &__layout
+        width: 100%
+        border-radius: 0
 
-@media (min-width: 691px)
-  .WAL
-    &__drawer-open
-      display: none
+  @media (min-width: 691px)
+    .WAL
+      &__drawer-open
+        display: none
 
-.conversation__summary
-  margin-top: 4px
+  .conversation__summary
+    margin-top: 4px
 
-.conversation__more
-  margin-top: 0!important
-  font-size: 1.4rem
-
-
+  .conversation__more
+    margin-top: 0!important
+    font-size: 1.4rem
 </style>
